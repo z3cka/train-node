d485f33b956eae822175be6a7e94cbabae1ff30e
Elliott Foster <elliottf@codebrews.com>
2012-09-12 10:45:09 -0500

    Adding more closure examples.

--- js101/closures/closures.js
+++ js101/closures/closures.js
@@ -15,6 +15,28 @@ var foo2 = function() {
 
 console.log(foo2());
 foo(bar, function closure(baz) {
-  console.trace();
   console.log("I'm a " + baz);
 });
+
+console.log('=============================');
+
+var example = function() {
+  var funcArray = {};
+  var i = 0;
+
+  // Define the functions.
+  for (i = 0; i < 3; i++) {
+    console.log('Inside: ' + i);
+    funcArray[i] = function() {
+      console.log('Outside: ' + i);
+    };
+  }
+
+  // Execute the functions.
+  funcArray[0]();
+  funcArray[1]();
+  funcArray[2]();
+};
+
+example();
+
