2f0b338e81b6639e80de8308f15b6a1aa90ae0d3
Elliott Foster <elliottf@codebrews.com>
2012-09-14 08:51:36 -0500

    Adding comments for scope example.

--- js101/scope/scope.js
+++ js101/scope/scope.js
@@ -2,63 +2,65 @@
  * @fileoverview scope deomonstration
  */
 
+/**
+ * EXAMPLE 1: global variable scope.
+ */
+console.log('=============================');
+
 var foo = 'kitteh';
 
+/**
+ * This function alters the 'foo' variable that was
+ * previously defined in the global scope.
+ */
 var example = function() {
   foo = 'doggie';
 };
 
-console.log('=============================');
 console.log('Example 1: foo = ' + foo);
 example();
 console.log('Now foo = ' + foo);
 
 console.log("\n");
 
+/**
+ * EXAMPLE 2: local variable scope.
+ */
+console.log('=============================');
+
+/**
+ * This function defines its own 'foo' variable that
+ * will not alter the one defined in the global scope.
+ */
 var example2 = function() {
   var foo = 'chupacabra';
   console.log("My foo = " + foo);
 };
 
-console.log('=============================');
 console.log('Example 2: foo = ' + foo);
 example2();
 console.log('Now foo = ' + foo);
 
 console.log("\n");
 
-// TODO - decide if we need this.
-var example3 = function() {
-  for (var i = 0; i < 5; i++) {
-    var x = 0;
-    console.log(x);
-    x++;
-  }
-  console.log(x);
-};
-
-var example3a = function() {
-  var x, i;
-  for (i = 0; i < 5; i++) {
-    x = 0;
-    console.log(x);
-    x++;
-  }
-  console.log(x);
-};
-
+/**
+ * EXAMPLE 3: hoisting.
+ */
 console.log('=============================');
-console.log('Example 3: no block scope');
-example3();
-example3a();
 
-var example4 = function() {
+/**
+ * This function attempts to access a variable before
+ * it is defined and illustrates hoisting.
+ *
+ * NOTE: if 'a' had not been defined within this scope,
+ * attempting to access it would have thrown a fatal error.
+ */
+var example3 = function() {
   console.log(a);
   var a = 1;
   console.log(a);
 };
 
-console.log('=============================');
-console.log('Example 4: hoisting');
-example4();
+console.log('Example 3: hoisting');
+example3();
 
