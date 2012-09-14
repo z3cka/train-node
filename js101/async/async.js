5ea7dd9f0088f87af95085d945bc42caa7d04987
Elliott Foster <elliottf@codebrews.com>
2012-09-14 09:32:43 -0500

    Adding more verbose output and comments to async example.

--- js101/async/async.js
+++ js101/async/async.js
@@ -1,3 +1,6 @@
+/**
+ * @fileoverview demonstrates asynchronous libraries.
+ */
 var fs = require('fs');
 
 var syncContents = '';
@@ -5,16 +8,19 @@ var asyncContents = '';
 
 console.log("I'm going to read a file synchronously.");
 
-// TODO - use system path separator.
-syncContents = fs.readFileSync(__dirname . '/closures.js');
+syncContents = fs.readFileSync(__filename);
 
 console.log("I'm done reading the file synchronously.");
+console.log('I read ' + syncContents.length + ' characters.');
 console.log("Now I'm ready to do the next thing.");
 
+console.log('=============================');
+
 console.log("I'm going to read a file asynchronously.");
-fs.readFile(__dirname . '/closures.js', function(err, data) {
+fs.readFile(__filename, function(err, data) {
   asyncContents = data;
   console.log("I'm done reading the file asynchronously.");
+  console.log('I read ' + asyncContents.length + ' characters.');
 });
 console.log("Now I'm ready to do the next thing.");
 
