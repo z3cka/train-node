116881b15472307acaefc99869f47a3f7a54f846
Elliott Foster <elliottf@codebrews.com>
2012-10-12 15:57:02 -0500

    Moving things around and making static files work.

--- exercises/server/app.js
+++ exercises/server/app.js
@@ -4,7 +4,6 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var poll = require('./lib/poll');
@@ -15,24 +14,17 @@ var app = express();
 
 app.configure(function() {
   app.set('port', config.port);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'client')));
 });
 
 app.configure('development', function() {
   app.use(express.errorHandler());
 });
-
-app.get('/', express.static(__dirname + '/client'));
-app.get('/app', express.static(__dirname + '/client/app'));
-app.get('/assets/css', express.static(__dirname + '/client/assets/css'));
-app.get('/assets/js', express.static(__dirname + '/client/assets/js'));
 
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
