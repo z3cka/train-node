d4cc2f38ba934fed929e44a5e29594e89b37782f
Elliott Foster <elliottf@codebrews.com>
2012-10-31 11:29:42 -0500

    Added comment to example file and giving the students res.writeHead().

--- hello/app.example.js
+++ hello/app.example.js
@@ -1,4 +1,13 @@
 var http = require('http');
+
+/**
+ * Creates an HTTP server with one response handler.
+ *
+ * @param {object} req
+ *   The HTTP request object.
+ * @param {object} res
+ *   The HTTP response object.
+ */
 http.createServer(function hello(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
--- hello/app.js
+++ hello/app.js
@@ -13,6 +13,7 @@ http.createServer(function hello(req, res) {
   // Hint: every response needs at least a HTTP header (res.writeHead())
   // and a call to res.end().
   // @see http://nodejs.org/docs/latest/api/http.html#http_class_http_serverresponse
+  res.writeHead(200, {'Content-Type': 'text/plain'});
 
   // TODO (bonus) - inspect the request object with console.log to find
   // client headers that got sent.
