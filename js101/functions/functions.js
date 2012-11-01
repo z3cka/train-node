803488e2ec6f4ebe70e568d9b6432ea623e35375
Michal Minecki <michal@mirzu.com>
2012-11-01 16:17:13 -0700

    added stacktrace example

--- js101/functions/functions.js
+++ js101/functions/functions.js
@@ -1,7 +1,5 @@
 /**
  * Define a function to be executed after the timeout.
- * Note the stack trace:
- *  at Object.namedFunc2...
  */
 var func = function() {
   console.log("I'm a different kind of named function.");
@@ -12,8 +10,6 @@ setTimeout(func, 1000);
 
 /**
  * Execute a named function after the timeout.
- * Note the stack trace:
- *   at Object.named [as _onTimeout]...
  */
 setTimeout(function named() {
   console.log("I'm named, somebody loves me.");
@@ -22,8 +18,6 @@ setTimeout(function named() {
 
 /**
  * Execute an anonymous function after the timeout.
- * Note the stack trace:
- *   at Object._onTimeout...
  */
 setTimeout(function() {
   console.log("I'm anonymous, see?");
@@ -36,44 +30,7 @@ setTimeout(function() {
  *  Using functions as callbacks is fundumental to how
  *  we write ansyncrous code.
  *
- *  It's the pattern we see all the time.
+ *  It's the pattern we'll use all day.
  *
  */
 console.log('This runs before all of the above!');
-
-
-/**
- *  Why is naming functions important?
- */
-
-/**
- * Define a function to be executed after the timeout.
- * Note the stack trace:
- *  at Object.namedFunc2...
- */
-var func = function() {
-  console.log("I'm a different kind of named function.");
-  console.log('==========================');
-};
-
-setTimeout(func, 1000);
-
-/**
- * Execute a named function after the timeout.
- * Note the stack trace:
- *   at Object.named [as _onTimeout]...
- */
-setTimeout(function named() {
-  console.log("I'm named, somebody loves me.");
-  console.log('==========================');
-}, 2000);
-
-/**
- * Execute an anonymous function after the timeout.
- * Note the stack trace:
- *   at Object._onTimeout...
- */
-setTimeout(function() {
-  console.log("I'm anonymous, see?");
-  console.log('==========================');
-}, 3000);
